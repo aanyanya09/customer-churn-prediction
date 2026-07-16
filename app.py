722528f5afd54137a188e8e986e75d3199a8057e
Aanyanya <aanya1892@gmail.com>
2026-07-16 05:33:58 +0530

    Added charts and improved UI

--- app.py
+++ app.py
@@ -143,7 +143,28 @@ if st.button("Predict Churn", type="primary"):
 
       else:
         st.success("🟢 Low Risk Customer")
+    importance = pd.DataFrame({
+    "Feature": columns,
+    "Importance": model.feature_importances_
+    })
 
+    importance = importance.sort_values(
+    by="Importance",
+    ascending=False
+    ).head(10)
+
+    fig, ax = plt.subplots(figsize=(8,5))
+
+    ax.barh(
+    importance["Feature"],
+    importance["Importance"]
+    )
+
+    ax.invert_yaxis()
+
+    st.subheader("⭐ Top 10 Important Features")
+
+    st.pyplot(fig)
     st.subheader("Result")
 
     if pred == 1:
